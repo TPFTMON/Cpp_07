2fb5de2b65d3f3eefbdf148ae2b355f493d69589
TPFTMON <tpftmoon@gmail.com>
2026-03-31 04:44:13 +0200

    ex00: subjects's main added

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,7 +2,6 @@
 
 int main(){
 
-
     int a = 42;
     int b = 80;
     std::cout << "a = " << a << ", b = " << b << "\n";
@@ -41,3 +40,25 @@ int main(){
 
     return (0);
 }
+
+
+
+// subject's main:
+// int main( void ) {
+//     int a = 2;
+//     int b = 3;
+
+//     ::swap( a, b );
+//     std::cout << "a = " << a << ", b = " << b << std::endl;
+//     std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
+//     std::cout << "max(a, b) = " << ::max( a, b ) << std::endl;
+
+//     std::string c = "chaine1";
+//     std::string d = "chaine2";
+
+//     ::swap(c, d);
+//     std::cout << "c = " << c << ", d = " << d << std::endl;
+//     std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
+//     std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
+//     return 0;
+// }
